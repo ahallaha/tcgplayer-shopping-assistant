d4560142cf32b5f39016569ca0ff9c55411d9243
Austin <hireaustinhall@gmail.com>
2022-07-12 22:35:22 -0400

    Update build script

--- build.ps1
+++ build.ps1
@@ -1,8 +1,12 @@
-Remove-Item ./dist.zip
+$ZipPath = "./dist.zip"
+
+if (Test-Path $ZipPath) {
+  Remove-Item $ZipPath
+}
 
 $compress = @{
   Path             = "./icons", "entry.html", "main.js", "manifest.json", "popup.js", "styles.css"
   CompressionLevel = "Fastest"
-  DestinationPath  = "./dist.zip"
+  DestinationPath  = $ZipPath
 }
 Compress-Archive @compress
